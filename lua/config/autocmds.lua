dfe6fbd110192670312c08200991f65d75bc9388
peyk0v <peykovn@gmail.com>
2024-09-24 02:07:24 -0300

    few changes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -27,6 +27,7 @@ vim.api.nvim_create_autocmd("Filetype", {
     "css",
     "lua",
     "astro",
+    "json",
   },
   command = "setlocal tabstop=2 shiftwidth=2 softtabstop=2",
   -- callback = function()
